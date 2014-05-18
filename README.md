c6f3d3675d44bd58f8379dc9f838c2779408e00d
Shaun Kelly <shaunkelly@gmail.com>
2014-05-18 17:24:26 -0400

    Tagging is now mostly working.

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
 # Pico Portfolio
 
-Pico Portfolio is a plugin and theme for the [Pico](http://picocms.org/) CMS that assists in creating a nice portfolio layout.
+Pico Portfolio is a plugin and theme for the [Pico CMS](http://picocms.org/) that assists in creating a nice portfolio layout.
+
+Or at least it will be when it's done
+
+## Features
+
+Pico Portfolio extends [Pico CMS](http://picocms.org/) by adding the following:
+
+* Allows you to arbitrarily order portfolio items using an order field in page meta data (instead of only by date or alpha).
+* Attach a default image to a portfolio item that can be accessed through the pages list.
+* Adds tagging so that you can tag portfolio items by role, type, etc. Get a list of portfolio items with a certain tag at yoursite.com/picodirectory/tags/thetag
+* [In progress] Includes a nice default portfolio theme that you can easily customize through SASS (or just use as an example to build your own).
+
+## Installation
+
+1. Install [Pico CMS](http://picocms.org/) first. Obvi.
+
+1. Copy pico_portfolio.php into the plugins directory.
+
+1. Copy the pico_portfolio theme folder into the themes directory.
+
+1. Add the following line to config.php:
+
+		$config['theme'] = 'pico_portfolio'; 
+
+## Use
+
+### Page meta data
+
+Pico Portfolio adds the following page meta data fields:
+
+	Portfolio: true
+	Order: 3
+	Tags: ixd usability dev research prototyping web
+	Image: image.png 
+	
+They do the following:
+
+* __Portfolio__ - Add this field to turn any page into a portfolio item page. Any value here will do. Pico Portfolio only checks for the presence of the field. In the default theme, any item that is not a portfolio page goes in the menu.
+* __Order__ - Add this field to arbitrarily order the list of portfolio item pages. Does not have to be 1, 2, 3 order. 10, 17, 3645 works just as well. Any portfolio items not marked with order will be ordered in the default ordering and put at the back of the list.
+* __Tags__ - Add this to tag items. Tags are space-delimited.
+* __Image__ - Default image for the portfolio item. Using the pico_portfolio theme, you should place this image in the themes/pico_portfolio/images folder.
+
+### Config
+
+Pico Portfolio adds the following fields to the config.php file:
+
+	$config['default_tag_file']
+	$config['site_owner']
+	
+They do the following:
+
+* default_tag_file - By deafult, the pico_portfolio theme displays requests for tags/whatever using the index.md file. If you want to use a different file (say, tags.md), specifiy that here.
+* site_owner - The pico_portfolio theme uses this value if it exists for the copyright statement in the footer.
+
+### Theme
+
+## TODO
+
+* /tags/ blank page should display all items, too
+* pass the list of requested tags to the template
+* Finish theme
--- plugins/pico_portfolio.php
+++ plugins/pico_portfolio.php
@@ -9,6 +9,10 @@
  */
 class Pico_Portfolio {
 
+	private $default_tag_file;
+	private $is_tag_request = false;
+	private $requested_tags;
+
 	public function plugins_loaded()
 	{
 		
@@ -16,7 +20,13 @@ class Pico_Portfolio {
 
 	public function config_loaded(&$settings)
 	{
-		
+		/* 
+			Set the file to use for tag requests 
+			Get from config or use index.md by default
+		*/
+		if(!isset($settings['default_tag_file']))
+			$settings['default_tag_file'] = 'index'. CONTENT_EXT;
+		$this->default_tag_file = CONTENT_DIR . $settings['default_tag_file'];
 	}
 	
 	public function request_url(&$url)
@@ -26,7 +36,19 @@ class Pico_Portfolio {
 	
 	public function before_load_content(&$file)
 	{
-		
+		/* 
+			If this is a tag request:
+			- get the tag requested
+			- set the content page to the index,
+			- set the is_tag_request for twig vars later,
+		*/
+		$tagdir = 'tags';
+		$len = strlen(CONTENT_DIR) + strlen($tagdir);
+		if(strncasecmp($file, CONTENT_DIR . $tagdir, $len) == 0) {
+			$this->requested_tags = substr($file, $len + 1, strlen($file) - ($len + 1) - strlen(CONTENT_EXT));
+			$file = $this->default_tag_file;
+			$this->is_tag_request = true;
+		}
 	}
 	
 	public function after_load_content(&$file, &$content)
@@ -41,7 +63,7 @@ class Pico_Portfolio {
 	
 	public function after_404_load_content(&$file, &$content)
 	{
-		
+
 	}
 	
 	public function before_read_file_meta(&$headers)
@@ -73,27 +95,38 @@ class Pico_Portfolio {
 		/* Apply meta tag fields to page data */
 		if(isset($page_meta['portfolio']))
 			$data['portfolio'] = $page_meta['portfolio'];
-		if(isset($page_meta['tags']))
-			$data['tags'] = $page_meta['tags'];
 		if(isset($page_meta['image']))
 			$data['image'] = $page_meta['image'];
 		if(isset($page_meta['order']))
-			$data['order'] = $page_meta['order'];		
+			$data['order'] = $page_meta['order'];
+		if(isset($page_meta['tags']))
+			$data['tags'] = explode(" ", $page_meta['tags']);
 	}
 	
 	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
 	{
 		/*
+			If this is a tag request, go through pages
+			and check to see if requested tag is included.
+			Only include those with tag in list.
+		*/
+		if($this->is_tag_request && strlen($this->requested_tags) > 0) {
+			foreach($pages as &$page) {
+				if(isset($page['tags']) && in_array($this->requested_tags, $page['tags']))
+					$page['has_requested_tag'] = true;
+			}
+		}
+
+		/*
 			Sort the array of pages by the value set in the order field.
-			Pages that don't have an order value will sit at the front of the array
-			in their original order.
+			Pages that don't have an order value will sit at the back of the array.
 		*/
 		$tmp = array();
-		foreach($pages as $page) {
-			if(isset($page['order'])) {
-				$tmp[] = $page['order'];
+		foreach($pages as $pg) {
+			if(!empty($pg['order'])) {
+				$tmp[] = $pg['order'];
 			} else {
-				$tmp[] = null;
+				$tmp[] = array(); // array always evaluates greater than a number
 			}
 		}
 		array_multisort($tmp, $pages);
@@ -104,9 +137,13 @@ class Pico_Portfolio {
 		
 	}
 	
+	
 	public function before_render(&$twig_vars, &$twig, &$template)
 	{
-		
+		/*
+			Adds is_tag_request to twig vars
+		*/
+		$twig_vars['is_tag_request'] = $this->is_tag_request;
 	}
 	
 	public function after_render(&$output)

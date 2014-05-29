b9737dc8a053ca195c86405f760fd70752ad1155
Shaun Kelly <shaunkelly@gmail.com>
2014-05-28 23:23:48 -0400

    Change portfolio item list to inline block to fix layout issues. Fixing spaceless issue in template. Lots of tweaks to css.

--- themes/pico_portfolio/js/tagswitcher.js
+++ themes/pico_portfolio/js/tagswitcher.js
@@ -18,7 +18,6 @@ function readyFunction() {
 	Hide or show as appropriate
 */
 function hideIfNoTag(tagfor) {
-	var numcols = getFrontPageColumns();
 	var itemnum = 0;
 	$(".portfolioitem").each(function() {
 		var tags = $(this).data("tags").split(" ");
@@ -30,20 +29,5 @@ function hideIfNoTag(tagfor) {
 			$(this).show(350);
 			itemnum++;
 		}
-		if (itemnum % (numcols + 1) == 0) {
-			$(this).css("clear", "both");
-		} else {
-			$(this).css("clear", "none");
-		}
 	});
-}
-
-/*
-	@todo This shouldn't hard code the margin/gutter values
-	@return the number of portfolio item columns on the front page
-*/
-function getFrontPageColumns() {
-	var tempcol = Math.floor(($(".portfolioitemlist").width() - 20) / $(".portfolioitem").width());
-	var gutter = (tempcol -1) * 20; // TODO - Don't hardcode the margin; also, breaks down if too many cols
-	return Math.floor(($(".portfolioitemlist").width() - 20 - gutter) / $(".portfolioitem").width());
 }
